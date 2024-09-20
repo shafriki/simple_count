35ddbbbc1ee89e01c323986be7ecd68da7aab297
shafriki <shafriki.cse@gmail.com>
2024-09-20 11:30:51 +0600

    complete

--- simple.js
+++ simple.js
@@ -1,3 +1,18 @@
 const htmlTag = document.getElementsByTagName("html")[0];
 
 htmlTag.setAttribute("data-theme", "light");
+
+const count = document.getElementById("count");
+
+
+function increaseCount() {
+let currentCount = Number(count.innerText)
+currentCount++;
+count.innerText = currentCount;
+}
+
+function decreaseCount() {
+    let currentCount = Number(count.innerText)
+currentCount--;
+count.innerText = currentCount;
+    }
